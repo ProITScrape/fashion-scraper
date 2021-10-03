c19091ca48a1d85390a482ac96b048a55381d8c5
Abdelouhab Bouasla <wahab2014@gmail.com>
2021-10-03 11:38:43 +0100

    edit

--- sources/sources/spiders/test_it.py
+++ sources/sources/spiders/test_it.py
@@ -6,15 +6,38 @@ from benedict import benedict
 class TestItSpider(scrapy.Spider):
     name = 'test_it'
    
-    start_urls = ['https://www.shein.com/Clothing-c-2030.html']
 
+    start_urls = ['https://www.shein.com/Clothing-c-2030.html']
+    def get_children(self,json_input,cats):
+        cat_id = json_input['cat_id']
+        if "children" in json_input.keys():
+            for e in json_input['children']:
+                cat_id = e['cat_id']
+                cats= self.get_children(e,cats)
+
+        else:
+            cats.append(cat_id)
+        return cats
     def parse(self, response):
         data=re.search(r'gbProductListSsrData = (.*?)\n', response.body.decode("utf-8")).group(1)
         data = json.loads(data)
         data=data['results']['filterCates']['children']
         for cat in data:
-            cat_id = cat['cat_id']
-        d = benedict(data)
-        cats =d.search('cat_id', in_keys=True, exact=True, case_sensitive=False)
-        print ("hhhh",cats)
+            cats = []
+            print(self.get_children(json_input,cats))
+            
+            
+            
+            """cat_id = cat['cat_id']
+            if "children" in cat.keys():
+                for cat_ch in cat['children']:
+                    cat_ch_id = cat_ch['cat_id']
+                    if "children" in cat_ch.keys():
+                        for cat_ch_ch in cat_ch['children']:
+                            cat_ch_ch_id = cat_ch_ch['cat_id']
+                            if "children" in cat_ch_ch.keys():
+                            for cat_ch_ch_ch in cat_ch_ch['children']:
+                                cat_ch_ch_ch_id = cat_ch_ch_ch['cat_id']
+            """            
+        
             
